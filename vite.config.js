78d21a8af7b59905b4d2a482c84c3e539a12d64e
Rahul-kal <127105474+Rahul-kal@users.noreply.github.com>
2025-07-10 23:02:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,3 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -9,4 +5,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    outDir: "dist"
+  }
+});
